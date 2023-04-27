0403d1e12fa86468ab53fc77e598518474e1bba4
Arpit <arpitmall02@gmail.com>
2023-04-27 23:29:51 +0530

    MongoDB & Express setup

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.1"
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI);
+}
+
+module.exports = connectToMongo;
+
